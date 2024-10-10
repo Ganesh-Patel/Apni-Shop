c46d3ecb6d2799e2af5365f539c8bf43eed43ad2
Ganesh-Patel-U <ganesh.oficial158@gmail.com>
2024-10-10 12:31:09 +0530

    register routing issue

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -118,7 +118,7 @@ function Header() {
             <FaTimes className="text-xl" />
           </button>
           <Link to="/login" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Login</Link>
-          <Link to="/register" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Register</Link>
+          <Link to="/signup" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Register</Link>
         </div>
       )}
 
@@ -148,7 +148,7 @@ function Header() {
             ) : (
               <div onClick={closeMobileMenu}>
                 <Link to="/login" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Login</Link>
-                <Link to="/register" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Register</Link>
+                <Link to="/signup" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Register</Link>
               </div>
             )}
           </div>
